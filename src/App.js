1a697d19b8023d2c120d0829f5ca1dc3cc2cbe9d
Rhitree <rhian.jenkins90@gmail.com>
2025-11-20 12:49:14 +0000

    Added footer and links to github and netlify

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="App">
+  return <div className="App">
       Weather App
+      <footer>
+        This project was coded by Rhian Jenkins and is open-sourced on 
+        <a href= "https://github.com/Rhitree/react-final-weather-app"> Github </a>
+        and hosted on 
+        <a href="https://silver-eclair-98aa41.netlify.app/"> Netlify </a>
+      </footer>
     </div>
-  );
+  ;
 }
 
