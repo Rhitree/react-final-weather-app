850b8b44f361cb36a7c787072b0afcfccb19a830
Rhitree <rhian.jenkins90@gmail.com>
2025-12-03 14:10:16 +0000

    Added CSS and basic set up of Weather App html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return <div className="App">
-      Weather App
+    <div className="outerContainer">
+    <div className="container">
+      <h1 className="mainHeading">Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Rhian Jenkins and is open-sourced on 
-        <a href= "https://github.com/Rhitree/react-final-weather-app"> Github </a>
+        <a href= "https://github.com/Rhitree/react-final-weather-app" target="_blank"> Github </a>
         and hosted on 
-        <a href="https://silver-eclair-98aa41.netlify.app/"> Netlify </a>
+        <a href="https://silver-eclair-98aa41.netlify.app/" target="_blank"> Netlify </a>
       </footer>
+      </div>
+      </div>
     </div>
   ;
 }
