104ef2edad1b3b4264b260f7932de1a8e87f17ef
Rhitree <rhian.jenkins90@gmail.com>
2025-12-29 14:56:19 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ export default function App() {
           </div>
       <footer>
         This project was coded by Rhian Jenkins and is open-sourced on 
-        <a href= "https://github.com/Rhitree/react-final-weather-app" target="_blank"> Github </a>
+        <a href= "https://github.com/Rhitree/react-final-weather-app" target="_blank" rel="noreferrer"> Github </a>
         and hosted on 
-        <a href="https://silver-eclair-98aa41.netlify.app/" target="_blank"> Netlify </a>
+        <a href="https://silver-eclair-98aa41.netlify.app/" target="_blank" rel="noreferrer"> Netlify </a>
       </footer>
       </div>
     </div>
