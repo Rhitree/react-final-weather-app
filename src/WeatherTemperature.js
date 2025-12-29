2a8cc120f0441d1fccca9a4bd5ce1e5bc4a6c74e
Rhitree <rhian.jenkins90@gmail.com>
2025-12-29 15:26:04 +0000

    Adjusting CSS to make project fit screen better

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -15,22 +15,22 @@ export default function WeatherTemperature(props) {
     if (unit === "celsius") {
     return (
         <div className="WeatherTemperature">
-            <span className="temperature">
+            <div className="temperature">
                 {Math.round(props.celsius)}
-            </span>
-            <span className="unit">°C|<a href="/" onClick={showFahrenheit}>°F</a>
-            </span>
+            </div>
+            <div className="unit">°C|<a href="/" onClick={showFahrenheit}>°F</a>
+            </div>
         </div>
     );
 } else {
     let fahrenheit = (props.celsius *9) /5 + 32;
     return (
         <div className="WeatherTemperature">
-            <span className="temperature">
+            <div className="temperature">
                 {Math.round(fahrenheit)}
-            </span>
-            <span className="unit"><a href="/" onClick={showCelsius}>°C</a>|°F
-            </span>
+            </div>
+            <div className="unit"><a href="/" onClick={showCelsius}>°C</a>|°F
+            </div>
         </div>
     );
 }
